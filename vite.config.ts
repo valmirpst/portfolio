7a749386e12ab91eb2794fb7d8deb95e75943db5
Valmir Paiva Stachin <valmir@unimake.com.br>
2026-02-10 20:38:39 -0300

    feat: add GitHub Actions workflow for deploying Vite app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/portfolio/" : "/",
+  base: "/portfolio/",
   server: {
     host: "::",
     port: 8080,
